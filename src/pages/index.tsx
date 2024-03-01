3af294b1b04ff06e4077af92d91e3544ef9a120e
awkbitgb <gonzalo.bedani@awkbit.com>
2024-03-01 19:06:19 +0000

    Update site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Page() {
             <br className="inline-block" /> ved
           </>
         }
-        t8="te"
+        t8="te2"
         t9={
           <>
             I&#xb4;m looking for a{" "}
